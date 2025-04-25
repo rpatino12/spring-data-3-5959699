f7eca0b8f828eae9c2d8cf23af4c1b8ad7ab8a2f
Ricardo Patino <90358329+rpatino12@users.noreply.github.com>
2025-04-25 22:26:41 +0000

    Migrate the application to Spring Boot and Spring Data, reducing a lot of boilerplate code

--- university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
+++ university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
@@ -1,26 +1,20 @@
 package com.example.university.dao;
 
-import com.example.university.PersistenceJPAConfig;
 import com.example.university.business.CourseFilter;
 import com.example.university.business.DynamicQueryService;
 import com.example.university.business.UniversityService;
 import com.example.university.domain.Department;
 import com.example.university.domain.Staff;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import static com.example.university.business.CourseFilter.filterBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test Criteria-based queries
  */
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { PersistenceJPAConfig.class })
+@SpringBootTest
 public class CriteriaQueryTest {
 
     @Autowired
